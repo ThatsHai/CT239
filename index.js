b7a87eb8efa45f5ed521e3c0d8d753230a42ea82
Hai Nguyen <nguyendonghaiwork94@gmail.com>
2024-01-28 02:39:45 +0700

    Fix login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
@@ -12,5 +13,9 @@ loginLink.addEventListener('click', ()=>{
 });
 
 btnPopup.addEventListener('click', ()=>{
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
 });
